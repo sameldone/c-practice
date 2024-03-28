8ce157f38a010f2c389e00187de3b98d22c1a760
Sameldone <sameldone1@gmail.com>
2024-03-28 11:15:21 +0300

    _putchar function using write()

--- C_Practice/0x02-functions_nested_loops/_putchar.c
+++ C_Practice/0x02-functions_nested_loops/_putchar.c
@@ -2,11 +2,12 @@
  * _putchar.c
  * _putchar()-Return 1 byte input
  * *****************************************************************/
-
+#include "sameldone.h"
 #include <unistd.h>
 
 int _putchar(char ch)
 {
-	return (write(1,&ch,1);
+
+	return (write(1,&ch,1));
 			
 }
---
+++ C_Practice/0x02-functions_nested_loops/sameldone.h
@@ -0,0 +1,8 @@
+#ifndef SAMELDONE_H
+#define SAMELDONE_H
+
+#include <stdio.h>
+int main();
+int _putchar(char ch);
+
+#endif

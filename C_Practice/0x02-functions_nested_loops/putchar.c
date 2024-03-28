455de3f222163ec218be775a0ba72a2c81d13c08
Sameldone <sameldone1@gmail.com>
2024-03-28 08:14:44 +0300

    putchar.c to print _putchar

---
+++ C_Practice/0x02-functions_nested_loops/putchar.c
@@ -0,0 +1,14 @@
+/******************************************************************
+ * putchar.c
+ * main()-Prints _putchar and a newline.
+ * Return-Always 0
+ * *****************************************************************/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}

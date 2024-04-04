43577b012459010343ddf016fb0c1a2fde7431cb
Sameldone <sameldone1@gmail.com>
2024-04-04 18:34:22 +0300

    strcpy corrections

---
+++ C_Practice/0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+/******************************************************************
+ * 100-atoi.c
+ * _atoi()-converts string to integer
+ * Return-integer value of the string
+ * *****************************************************************/
+
+#include "main.h"
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+
+	} while (*s++);
+
+	return (num * sign);
+}
